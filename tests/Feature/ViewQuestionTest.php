2cdd24cfe0b8aba34136b0d291bcbdf4d406f9eb
Hongyuan <hongyuan@fstln.io>
2020-11-29 18:20:45 +0800

    test view single question

--- tests/Feature/ViewQuestionTest.php
+++ tests/Feature/ViewQuestionTest.php
@@ -2,18 +2,34 @@
 
 namespace Tests\Feature;
 
+use App\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ViewQuestionTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function user_can_view_questions()
     {
-        $this->withoutExceptionHandling();
-        
         $test = $this->get('/questions');
 
         $test->assertStatus(200);
+    }
+    
+    /** @test */
+    public function user_can_view_a_single_question()
+    {
+        // create a question
+        $question = factory(Question::class)->create();
+
+        // show the question
+        $test = $this->get('/questions/' . $question->id);
+        // see the content of the question
+
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
